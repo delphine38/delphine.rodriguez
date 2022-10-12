f574b79fdbe98801c512a62e6b6765227822eaa8
delphine <delphin38@live.fr>
2022-10-12 22:12:29 +0200

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,90 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+// import Link from 'next/link';
+
+// intéraction et animation avec 
+// "useState" et 
+// "useEffect" pour lorsque le composant est chargé
+
 import velo from "../images/velo.jpg";
 
 
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-        <div className='navbarFlex'>
-            <article className='d-flex'>
-                <img src= {velo} alt="vélo dans un ordinateur" width="100px" height="100px" />
 
-                <article>
-                    <h1>Delphine RODRIGUEZ | Développeuse web Junior</h1>
+const [toggleMenu, setToggleMenu] = useState(false);
+const [largeur, setLargeur] = useState(window.innerWidth);
+
+const toggleNavSmallScreen = () =>{
+    setToggleMenu(!toggleMenu);
+}
+
+useEffect(() =>{
+
+    const changeWidth = () => {
+        setLargeur(window.innerWidth);
+
+        if(window.innerWidth >500 ){
+            setToggleMenu(false);
+        }
+    }
+
+    window.addEventListener('resize', changeWidth);
+
+    return () => {
+        window.removeEventListener('resize', changeWidth);
+    }
+
+}, [] )
+
+
+  return (
+<div>
+    <nav>
+        
+        <article className='nav'>
+            <img src= {velo} alt="vélo dans un ordinateur" width="100px" height="100px" />
+            {/* <article> */}
+                        <h1 className='mt-5'>Delphine RODRIGUEZ | Développeuse web Junior</h1>
+                    {/* </article> */}
+        </article>
+        {(toggleMenu || largeur > 500) && (
+            <ul className='list'>
+                <a href='/#home'><li className='items'>Accueil</li></a>
+                <a href='/#realisations'><li className='items'>Réalisations</li></a>
+                <a><li className='/#competences'>Compétences</li></a>
+            {/* <li className='items'>Accueil</li>
+            <li className='items'>Réalisations</li>
+            <li className='items'>Compétences</li> */}
+        </ul>
+        ) }
+        
+        {/* <button className='btn'>X</button> */}
+        <button onClick={toggleNavSmallScreen} className='btn'>X</button>
+    </nav>
+
+    {/* <div className='container'>
+        <div className='navbar'>
+            <div className='navbarFlex'>
+                <article className='d-flex'>
+                    <img src= {velo} alt="vélo dans un ordinateur" width="100px" height="100px" />
+
+                    <article>
+                        <h1>Delphine RODRIGUEZ | Développeuse web Junior</h1>
+                    </article>
                 </article>
-            </article>
-            
-            
-            <div>
-                <ul>
-                    <li><a href="/">Accueil</a></li>
-                    <li><a href="">Réalisations</a></li>
-                    <li><a href="">Compétences</a></li>
-                    <li><a href="">A propos de moi</a></li>
-                    <li><a href="">Contact</a></li>
-                </ul>
+                
+                
+                <div>
+                    <ul>
+                        <li><a href="/">Accueil</a></li>
+                        <li><a href="">Réalisations</a></li>
+                        <li><a href="">Compétences</a></li>
+                        <li><a href="">A propos de moi</a></li>
+                        <li><a href="">Contact</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
+    </div> */}
     </div>
   )
 }
--- src/components/Realisations.js
+++ src/components/Realisations.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import gitHub from '../images/skills/github.png';
 import votreAge from '../images/projects/votreage.jpg';
 import theaAndMom from '../images/projects/theaandmom.jpg';
 import crudJavascript from '../images/projects/crudjavascript.jpg';
+import mathsJs from '../images/projects/mathsjs.jpg';
+import compteurReact from '../images/projects/compteurreact.jpg';
 
 
 import html from '../images/skills/html.jpg';
@@ -9,7 +12,7 @@ import html from '../images/skills/html.jpg';
 
 const Realisations = () => {
   return (
-    <div className='realisationsDiv'>
+    <div className='realisationsDiv' id='#realisations'>
         <p>Réalisations</p>
       <h2>Les projets que j'ai réalisés</h2>
 
@@ -21,32 +24,46 @@ const Realisations = () => {
 
 
           {/* découvre ton age en jours */}
-          <div className='realisations col-3'>
-            <div className='realisationsRelative'>
-                {/* <a href={"https://votreage.netlify.app/"} className="aImageOpacity" target="_blank"> */}
-                  <img src={votreAge} alt="application pour découvrir ton âge en jours" width="90%"/>
-                {/* </a>    */}
+            <div className='realisations col-3'>
+              <div className='realisationsRelative'>
+                  {/* <a href={"https://votreage.netlify.app/"} className="aImageOpacity" target="_blank"> */}
+                    <img src={votreAge} alt="application pour découvrir ton âge en jours" width="90%"/>
+                  {/* </a>    */}
+              </div>
+              
+              <div className='hidden'>
+                <a href={"https://votreage.netlify.app/"} className="aImageOpacity" target="_blank">
+                  <h3>Application : votre âge <br/> pour connaitre votre age en jours</h3>
+                  <p>Javascript</p> 
+                </a>
+                <a href={"https://github.com/delphine38/age"} target="_blank">
+                  <img src={gitHub} alt="gitHub de ton âge en jours" width="60px" />
+                </a>
+
+              </div>
+              
             </div>
-            <a href={"https://votreage.netlify.app/"} className="aImageOpacity" target="_blank">
-            <div className='hidden'>
-                <h3>Application : votre âge <br/> pour connaitre votre age en jours</h3>
-                <p>Javascript</p> 
-            </div>
-            </a>
-          </div>
+            
+          
+        
 
 
           {/* Théa and mom's création */}
-          <div className='realisations col-3'>
-            <div className='realisationsRelative'>
-                  <img src={theaAndMom} alt="web site théa and mom's création" width="90%"/>
-            </div>
-            <a href={"https://theaandmomcreations.netlify.app/"} className="aImageOpacity" target="_blank">
-              <div className='hidden'>
-                  <h3>Web site en cours de Théa and Mom's <br /> (La maman crée des bijoux avec sa fille)</h3>
+          
+            <div className='realisations col-3'>
+              <div className='realisationsRelative'>
+                    <img src={theaAndMom} alt="web site théa and mom's création" width="90%"/>
               </div>
-            </a>
-          </div>
+              <a href={"https://theaandmomcreations.netlify.app/"} className="aImageOpacity" target="_blank">
+                <div className='hidden'>
+                    <h3>Web site en cours de Théa and Mom's <br /> (La maman crée des bijoux avec sa fille)</h3>
+                    <p>React</p>
+                    <a href={"https://github.com/delphine38/theaandmomreact"} target="_blank">
+                      <img src={gitHub} alt="gitHub de thea and mom's en react" width="60px" />
+                    </a>
+                </div>
+              </a>
+            </div>
 
 
           {/* crud Javascript */}
@@ -57,13 +74,48 @@ const Realisations = () => {
             <a href={"https://createreadupdatedeletejavascript.netlify.app/"} className="aImageOpacity" target="_blank">
               <div className='hidden'>
                   <h5>Exercice en javascript pour lister les missions à réalisés</h5>
+                  <p>Javascript</p>
+                  <a href={"https://github.com/delphine38/CreateReadUpdateDelete"} target="_blank">
+                    <img src={gitHub} alt="gitHub de C.R.U.D. en javascript" width="60px" />
+                  </a>
               </div>
             </a>
           </div>
 
+          {/* testdecalculjavascript */}
+          <div className='realisations col-3'>
+            <div className='realisationsRelative'>
+                  <img src={mathsJs} alt="revision de maths" width="90%"/>
+            </div>
+              <a href={"https://github.com/delphine38/testdecalculjavascript"} className="aImageOpacity" target="_blank">
+              <div className='hidden'>
+                  <h5>Application pour réviser ses Maths</h5>
+                  <p>Javascript</p>
+                  <a href={"https://github.com/delphine38/testdecalculjavascript"} target="_blank">
+                    <img src={gitHub} alt="gitHub de stickers" width="60px" />
+                  </a>
+              </div>
+            </a>
+          </div>
+
+          {/* compteurreact */}
+          <div className='realisations col-3'>
+            <div className='realisationsRelative'>
+                  <img src={compteurReact} alt="un compteur pour ajouter ou enlever" width="90%"/>
+            </div>
+              <a href={"https://compteurreact.netlify.app/"} className="aImageOpacity" target="_blank">
+              <div className='hidden'>
+                  <h5>Application en React.js pour ajouter ou enlever</h5>
+                  <p>React.js</p>
+                  <a href={"https://github.com/delphine38/compteurreact"} target="_blank"><img src={gitHub} alt="gitHub de stickers" width="60px" />
+                  </a>
+              </div>
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+                        </div>
+
   )
 }
 
